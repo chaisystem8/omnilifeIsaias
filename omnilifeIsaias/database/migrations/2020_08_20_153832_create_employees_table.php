7a744f3a36f60ba4351f3cc8bb1a7264843b45b3
chaisystem8 <62084319+chaisystem8@users.noreply.github.com>
2020-08-20 11:03:30 -0500

    init
    model, migrate, auth

---
+++ omnilifeIsaias/database/migrations/2020_08_20_153832_create_employees_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo',40);
+            $table->string('nombre',120);
+            $table->double('salarioDolores');
+            $table->double('salarioPesos');
+            $table->string('direccion',40);
+            $table->string('ciudad',40);
+            $table->string('telefono',40);
+            $table->string('correo',40);
+            $table->tinyInteger('activo')->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
